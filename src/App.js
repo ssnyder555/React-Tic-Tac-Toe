0ed973fe3453c61cfdc7e30ecbe31f642ddffc0e
ssnyder555 <ssnyder555@live.com>
2019-01-11 13:58:12 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      board: Array(9).fill(null),
+
+      player: "X"
+    }
+  }
+
+  handleClick(index){
+    let newBoard = this.state.board
+
+    if(this.state.board[index] === null) {
+      newBoard[index] = this.state.player
+
+          this.setState({
+            board: newBoard,
+
+            player: this.state.player === "X" ? "0" : "X"
+          });
+
+    }
+
+
+
+
+  }
   render() {
+     const Box = this.state.board.map(
+       (box, index) =>
+
+      <div className="box"
+
+        key={index} onClick={(e) =>
+
+          this.handleClick(index)}>{box}</div>)
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <div className="container">
+
+        <h1>tic-tac-toe app</h1>
+
+          <div className="board">
+            {Box}
+
+          </div>
       </div>
     );
   }
